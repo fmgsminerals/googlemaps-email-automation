ece5241d51e5b1f08faae34ef66ae0909d3a6ff1
fmgsminerals <126920713+fmgsminerals@users.noreply.github.com>
2025-09-10 02:56:13 +0500

    Update send_with_ses.py

--- scripts/send_with_ses.py
+++ scripts/send_with_ses.py
@@ -1,1 +1,14 @@
+import os, time, boto3
 
+
+ses = boto3.client("sesv2", region_name=os.environ["AWS_REGION"])
+
+
+def send_email(to_addr, subject, html):
+ses.send_email(
+FromEmailAddress=os.environ["SES_SENDER"],
+Destination={"ToAddresses":[to_addr]},
+Content={"Simple":{"Subject":{"Data":subject},"Body":{"Html":{"Data":html}}}},
+EmailTags=[{"Name":"segment","Value":"auto"}]
+)
+time.sleep(0.2)
